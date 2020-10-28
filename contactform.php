db61bb0f5ee0104b5414bd92e7bb75b2397e54a1
dddCddd <the.gargamel.organisation@gmail.com>
2020-10-28 19:08:50 +0000

    string

--- contactform.php
+++ contactform.php
@@ -1,15 +1,15 @@
 <?php
 
-if(isset($_POST ["submit"])){
-    $name = $_POST ["name"];
-    $mailFrom = $_POST ["email"];
-    $message = $_POST ["message"];
+if(isset($_POST ['submit'])){
+    $name = $_POST ['name'];
+    $mailFrom = $_POST ['email'];
+    $message = $_POST ['message'];
 
-    $mailTo = "rickard@betydlig.se";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have reccived an e-mail from" .$name.".\n\n".$message;
+    $mailTo = 'rickard@betydlig.se';
+    $headers = 'From: '.$mailFrom;
+    $txt = 'You have reccived an e-mail from' .$name.'.\n\n'.$message;
     
     mail($mailTo, $message, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header('Location: index.php?mailsend');
 
 }
