6796052f62e2805aee4720691604d5ef530c6a62
dddCddd <the.gargamel.organisation@gmail.com>
2020-10-28 19:21:04 +0000

    closing down

--- contactform.php
+++ contactform.php
@@ -6,10 +6,11 @@ if(isset($_POST ['submit'])){
     $message = $_POST ['message'];
 
     $mailTo = 'rickard@betydlig.se';
-    $headers = 'From: '.$mailFrom;
+    $headers = 'From:'.$mailFrom;
     $txt = 'You have reccived an e-mail from' .$name.'.\n\n'.$message;
     
     mail($mailTo, $message, $txt, $headers);
     header('Location: index.php?mailsend');
 
 }
+?>
